26a273a614e51cc20a6682936ba4739c6357d99c
Marek Bruchatý <marekbruchaty@gmail.com>
2017-04-13 11:21:10 +0200

    Independent servlet for viewing visitor counter

--- src/CounterViewServlet.java
+++ src/CounterViewServlet.java
@@ -1,4 +1,3 @@
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,25 +9,15 @@ import java.io.PrintWriter;
 /**
  * Created by Marek on 13/04/2017.
  */
-@WebServlet(name = "CounterServlet")
-public class CounterServlet extends HttpServlet {
-
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-        Integer counter = 0;
-        getServletContext().setAttribute("counter", counter);
-    }
-
+@WebServlet(name = "CounterViewServlet")
+public class CounterViewServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Integer counter = (Integer) getServletContext().getAttribute("counter");
-        counter++;
-
-        getServletContext().setAttribute("counter", counter);
+        if (counter == null) counter = 0;
 
         response.setContentType("text/html");
         response.setCharacterEncoding("UTF-8");
@@ -38,11 +27,11 @@ public class CounterServlet extends HttpServlet {
             writer.println("<!DOCTYPE html><html>");
             writer.println("<head>");
             writer.println("<meta charset=\"UTF-8\" />");
-            writer.println("<title>CounterServlet using doGet() to increment local variable</title>");
+            writer.println("<title>CounterViewServlet using doGet() to view counter value</title>");
             writer.println("</head>");
             writer.println("<body>");
 
-            writer.println("<h1>You are our " + counter + ". customer</h1>");
+            writer.println("<h1>Total number of visitors is "+ counter +"</h1>");
 
             writer.println("</body>");
             writer.println("</html>");
--- web/index.jsp
+++ web/index.jsp
@@ -8,14 +8,14 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
-    <title>$Title$</title>
+    <title>Simple Java Web App Demo</title>
   </head>
   <body>
     <h1>Simple Java Web App Demo</h1>
     <h2>Example #1 - Generate website using servlet</h2>
     <p>To invoke a simple Java servlet click <a href="SimpleServlet">here</a></p>
     <h2>Example #2 - Pass data to servlet</h2>
-    <p>To invoke Java servlet and pass data to it fill the form below and hit the "Submit" button.</p>
+    <p>To invoke Java servlet and pass data to it fill the form below and hit the "Submit" button</p>
     <br/>
     <form action="FormServlet" method="POST">
       First Name: <input type="text" name="first_name">
@@ -25,5 +25,7 @@
     </form>
     <h2>Example #3 - Access website with visitor count tracking</h2>
     <p>To invoke the counting servlet click <a href="CounterServlet">here</a></p>
+    <h2>Example #4 - Show total number of visits</h2>
+    <p>To invoke the servlet click <a href="CounterViewServlet">here</a></p>
   </body>
 </html>
