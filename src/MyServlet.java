642c1876d9fb20d35465e3f248a0e35ded795ed0
Marek Bruchatý <marekbruchaty@gmail.com>
2017-04-13 03:26:21 +0200

    Simple web page generated by java servlet

---
+++ src/MyServlet.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Marek on 13/04/2017.
+ */
+@javax.servlet.annotation.WebServlet(name = "MyServlet")
+public class MyServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+
+        try (PrintWriter writer = response.getWriter()) {
+
+            writer.println("<!DOCTYPE html><html>");
+            writer.println("<head>");
+            writer.println("<meta charset=\"UTF-8\" />");
+            writer.println("<title>MyServlet.java:doGet(): Servlet code!</title>");
+            writer.println("</head>");
+            writer.println("<body>");
+
+            writer.println("<h1>This is a simple java servlet.</h1>");
+
+            writer.println("</body>");
+            writer.println("</html>");
+        }
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Marek
+  Date: 13/04/2017
+  Time: 02:49
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+    <h1>Simple Java Web App Demo</h1>
+    <p>To invoke a simple java servlet click <a href="MyServlet">here</a></p>
+  </body>
+</html>
